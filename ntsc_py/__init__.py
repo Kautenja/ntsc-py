2fb27ce84463858f0fc6f7644b694d8a2fbfd769
Kautenja <ckfour@icloud.com>
2021-04-23 01:38:59 -0500

    update interface

--- ntsc_py/__init__.py
+++ ntsc_py/__init__.py
@@ -1,6 +1,14 @@
-"""the NTSC package."""
-from .nes_ntsc import NES_NTSC
+"""the NTSC graphical filter / shader package."""
+from .nes_ntsc import NES_NTSC, rgb2nes, nes2rgb
+from .snes_ntsc import SNES_NTSC, rgb32_888_to_rgb16_565, rgb16_565_to_rgb32_888
 
 
 # explicitly define the outward facing API of the package
-__all__ = [NES_NTSC.__name__]
+__all__ = [
+    NES_NTSC.__name__,
+    SNES_NTSC.__name__,
+    nes2rgb.__name__,
+    rgb2nes.__name__,
+    rgb16_565_to_rgb32_888.__name__,
+    rgb32_888_to_rgb16_565.__name__,
+]
