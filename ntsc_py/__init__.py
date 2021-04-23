5e034ae9b01d94d79eb80bac60e2fb77320445a1
Kautenja <kautencreations@gmail.com>
2021-04-22 22:15:02 -0500

    fix main __init__.py:
    remove unnecessary module import

--- ntsc_py/__init__.py
+++ ntsc_py/__init__.py
@@ -1,9 +1,6 @@
 """the NTSC package."""
-from . import nes_ntsc
 from .nes_ntsc import NES_NTSC
 
+
 # explicitly define the outward facing API of the package
-__all__ = [
-    nes_ntsc.__name__,
-    NES_NTSC.__name__,
-]
+__all__ = [NES_NTSC.__name__]
