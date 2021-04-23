caedc86b6b5c576462c10d236ce170b6a67f1818
Kautenja <kautencreations@gmail.com>
2021-04-22 22:13:37 -0500

    fix package header

--- ntsc_py/__init__.py
+++ ntsc_py/__init__.py
@@ -1,5 +1,9 @@
 """the NTSC package."""
-
+from . import nes_ntsc
+from .nes_ntsc import NES_NTSC
 
 # explicitly define the outward facing API of the package
-__all__ = []
+__all__ = [
+    nes_ntsc.__name__,
+    NES_NTSC.__name__,
+]
